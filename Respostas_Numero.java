e1705c19f4e935537f6a52a22bf26fd6d2a72caa
RdsMachado1181 <104657949+RdsMachado1181@users.noreply.github.com>
2025-07-22 21:06:07 -0300

    Create Respostas_Numero.java

---
+++ Respostas_Numero.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project._num;
+
+/**
+ *
+ * @author rdsmachado
+ */
+public class Respostas_Numero {
+    
+    public void resposta( Inserir_numero numero){
+        
+        int numero_resposta = numero.getNumero();
+        System.out.println(" O numero escolhidop foi " + numero_resposta);  
+    }
+
+    
+  
+    }
+ 
