fe022ee603c6e859abe13a064443b6fe186caefe
RdsMachado1181 <104657949+RdsMachado1181@users.noreply.github.com>
2025-07-22 21:05:46 -0300

    Create Resposta_erros.java

---
+++ Resposta_erros.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project._num;
+
+/**
+ *
+ * @author rdsmachado
+ */
+public class Resposta_erros {
+    
+    public void erros_resp( Inserir_numero erro_Resp ){
+       erro_Resp.getNumero();
+        
+        System.out.println(" O numero digitado esta incorreto, por favor digite um valor valido");
+      erro_Resp.Inserir_numero(  );
+    }
+}
