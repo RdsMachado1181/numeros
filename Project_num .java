4dd7a2c6c52f3d58f776cfdbbfa112eecc5f9521
RdsMachado1181 <104657949+RdsMachado1181@users.noreply.github.com>
2025-07-22 21:05:00 -0300

    Create Project_num .java

---
+++ Project_num .java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project._num;
+
+/**
+ *
+ * @author rdsmachado
+ */
+public class Project_num {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+         Inserir_numero numr01 = new  Inserir_numero();
+                 
+        System.out.println(" Digite um numero valido");
+        
+       numr01.Inserir_numero(   );
+    }
+    
+}
